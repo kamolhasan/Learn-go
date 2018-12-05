b4a01083ad7047c31ec70f0155f29f8a2e2bb3f8
Kamol Hasan <kamol@appscode.com>
2018-12-05 17:00:28 +0600

    added loop.go

---
+++ loop.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	for i < 10 {
+		fmt.Print(i)
+		fmt.Print(" ")
+		i = i + 1
+	}
+	fmt.Println()
+
+	for j := 10; j > 0; j-- {
+		if j%2 == 0 {
+			fmt.Printf("Even number %d\n", j)
+		} else if j%3 == 0 {
+			fmt.Printf("Multiple of 3 = %d \n", j)
+		} else {
+			fmt.Printf("simple Odd number %d\n",j)
+		}
+	}
+
+}
