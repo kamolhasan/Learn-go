f85cf0a90b0915fc60074b158769d7d620926821
Kamol Hasan <kamol@appscode.com>
2018-12-05 13:52:49 +0600

    String added

--- hello.go
+++ hello.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("hello masud")
-	fmt.Println("1+1 = ",1+1)
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("1+1.5 = ", 1.0+1.5)
+	fmt.Println("5/3 = ", 5.0/3)
+	s := "Good "
+	var m string = "Morning"
+	fmt.Println(s + m)
+	for i:=0;i<len(s);i++ {
+		fmt.Print(string(s[i]))
+	}
 
 }
