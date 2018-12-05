83421676f6e4f3ad461f25c92f4fc8864cb21ff8
Kamol Hasan <kamol@appscode.com>
2018-12-05 17:12:22 +0600

    switch statement added

--- loop.go
+++ loop.go
@@ -17,11 +17,24 @@ func main() {
 		} else if j%3 == 0 {
 			fmt.Printf("Multiple of 3 = %d \n", j)
 		} else {
-			fmt.Printf("simple Odd number %d\n",j)
+			fmt.Printf("simple Odd number %d\n", j)
 		}
 	}
 
-	if (true && true)||(false && true){
+	if (true && true) || (false && true) {
 		fmt.Println("always true")
+	}
+
+	var k int
+	fmt.Scanf("%d", &k)
+	switch k {
+	case 0:
+		fmt.Println("zero")
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	default:
+		fmt.Println("greater than two")
 	}
 }
