0fc9ce6cbc0ccb3806b6425227c32ad0544cdfbb
Kamol Hasan <kamol@appscode.com>
2018-12-06 17:06:02 +0600

    Closure added

---
+++ closure.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	add := func(a, b int) int {
+		return a + b
+	}
+	fmt.Println(add(1, 2))
+	a := 1
+	incriment := func() int {
+		a++
+		return a
+	}
+	fmt.Println(incriment())
+	fmt.Println(incriment())
+	fmt.Println(incriment())
+	
+	//closure (variable+function)
+	nxtEven := evenGen()
+	fmt.Println(nxtEven())
+	fmt.Println(nxtEven())
+	fmt.Println(nxtEven())
+	fmt.Println(nxtEven())
+}
+
+func evenGen() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
